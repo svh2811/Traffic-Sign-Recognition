0c5eae8019f4b885bb9d5e811e3ab3a154430a38
Sarvesh Upadhyay <sarvesh.a.upadhyay@gmail.com>
2018-07-30 21:13:44 -0400

    Insert: first commit

---
+++ constants.py
@@ -0,0 +1,15 @@
+base_directory = "./data"
+training_dir = base_directory + "/GTSRB/Final_Training/Images"
+
+test_dir = base_directory + "/GTSRB/Final_Test/Images"
+
+web_eg_dir = base_directory + "/web-examples"
+
+tensorboard_log_dir = "./tb_logs"
+
+base_model_dir = "./model"
+model_chkpt_file = base_model_dir + "/checkpoints/model_chkpt.h5"
+train_hist_file = base_model_dir + "/train_history/train_history_dict.p"
+
+base_plots_dir = "./plots"
+web_example_plots_dir = base_plots_dir + "/web-example-predictions"
